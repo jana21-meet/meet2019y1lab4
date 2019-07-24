dc878d8d7ed502678b196726731288b4e50331d5
jana21-meet <jana21@meet.mit.edu>
2019-07-24 09:57:43 -0400

    Stopping here

---
+++ fruit_sorter.py
@@ -0,0 +1,10 @@
+fruit=input("what is the fruit's kind??")
+if fruit=="apple":
+    print("bin1")
+elif fruit=="oranges":
+    print("bin2")
+elif fruit=="bananas":
+    print("bin3")
+else:
+    print("error I do not recognize this fruit")
+
---
+++ script_a.py
@@ -0,0 +1,6 @@
+day_num=input("enter the day's number")
+if (day_num>="0" and day_num<="4"):
+    print("the alarm is on 7:00")
+elif(day_num=="5" or day_num=="6"):
+    print ("the alarm is on 10:00")
+    
